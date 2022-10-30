32f9c757085254ab4ee08d1b9eaebcffe0467792
mcnanton <mcnanton@gmail.com>
2022-10-29 21:52:14 -0300

    fix menor

--- src/experimento_colectivo/fi_defecto/z925_FE_historia.r
+++ src/experimento_colectivo/fi_defecto/z925_FE_historia.r
@@ -27,7 +27,7 @@ PARAM$lag1  <- TRUE
 PARAM$lag2  <- FALSE
 PARAM$Tendencias  <- TRUE
 PARAM$RandomForest  <- FALSE          #No se puede poner en TRUE para la entrega oficial de la Tercera Competencia
-PARAM$CanaritosAsesinos  <- FALSE
+PARAM$CanaritosAsesinos  <- TRUE
 # FIN Parametros del script
 
 #------------------------------------------------------------------------------
